a54635d20c114ffcbe4c53e7cf5d7c38314660e7
Tiezzi Junior <marco.tiezzi@sou.unifeob.edu.br>
2025-01-07 15:01:18 -0300

    Create addition.c

---
+++ C/introducao/addition.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+/*
+//Tipo INT
+int main(void)
+{
+    int x = get_int("X: ");
+
+    int y = get_int("Y: ");
+
+    printf("%i\n", x + y);
+}
+*/
+
+//Tipo LONG
+int main(void)
+{
+    long x = get_long("X: ");
+
+    long y = get_long("Y: ");
+
+    printf("%li\n", x + y);
+}
