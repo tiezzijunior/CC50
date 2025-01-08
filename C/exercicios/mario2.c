52c381003218fda5c644f657709b64dcc6a60bcb
Tiezzi Junior <marco.tiezzi@sou.unifeob.edu.br>
2025-01-08 13:18:01 -0300

    Create mario2.c

---
+++ C/exercicios/mario2.c
@@ -0,0 +1,57 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Funcoes
+int get_height(void);
+void make_tower(int n);
+
+int main(void)
+{
+    // Prompt the height fo the tower
+    int height = get_height();
+
+    // Create the tower
+    make_tower(height);
+}
+
+int get_height(void)
+{
+    int height;
+
+    // Check if the heigth is greater than 0 and less than 9
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8);
+
+    return height;
+}
+
+void make_tower(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // Defines the floor
+        int andar = n - i;
+
+        for (int j = 1; j < andar; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < 2; k++)
+        {
+            for (int l = 0; l <= i; l++)
+            {
+                printf("#");
+            }
+
+            if (k == 0)
+            {
+                printf("  ");
+            }
+        }
+
+        printf("\n");
+    }
+}
